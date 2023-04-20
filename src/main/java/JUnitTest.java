3cce1815c6e56642b071b998dccbafd9e7f6a8aa
paffen <pavel.stoyanov0@hm.edu>
2023-04-20 08:02:45 +0200

    add some code

--- src/main/java/JUnitTest.java
+++ src/main/java/JUnitTest.java
@@ -1,2 +1,39 @@
-package PACKAGE_NAME;public class JUnitTest {
+public class JUnitTest {
+    public static void main(String[] args) {
+        int zahl1 = 20;
+        int zahl2 = 50;
+        System.out.println("Summe ist: " + add(zahl1, zahl2));
+        System.out.println("FizzBuzz: " + FizzBuzz(40));
+        System.out.println("SmallerThen: " + smallerThanZero(40));
+
+    }
+
+    public static int add(int a, int b) {
+        return a + b;
+    }
+
+
+    public static String FizzBuzz(int num) {
+        int num1 = 3;
+        int num2 = 5;
+        if (isTeilbar(num, num1) && isTeilbar(num, num2)) {
+            return "fizzbuzz";
+        }
+        if (isTeilbar(num, num1)) {
+            return "fizz";
+        }
+        if (isTeilbar(num, num2)) {
+            return "buzz";
+        }
+            return String.valueOf(num);
+    }
+
+    public static boolean isTeilbar(int eins, int durch) {
+        return eins % durch == 0;
+    }
+
+    public static boolean smallerThanZero(int num) {
+        //check if number is positive or negative
+        return num < 0;
+    }
 }
--- src/test/java/JUnitTestTest.java
+++ src/test/java/JUnitTestTest.java
@@ -1,4 +1,43 @@
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 class JUnitTestTest {
-  
+
+    @Test
+    void checkEquals_100(){
+        int a = 50;
+        int b = 50;
+        int expected = a + b;
+        int actual = JUnitTest.add(a,b);
+
+        assertEquals(actual,expected);
+        assertTrue(actual==expected);
+    }
+        @Test
+        public void testDividedByThreeAndFivePrintsFizzBuzz() {
+            assertEquals("fizzbuzz", JUnitTest.FizzBuzz(15));
+        }
+        @Test
+        public void testDividedByThreeOnlyPrintsFizz() {
+            assertEquals("fizz", JUnitTest.FizzBuzz(3));
+        }
+        @Test
+        public void testDividedByFiveOnlyPrintsBuzz() {
+            assertEquals("buzz", JUnitTest.FizzBuzz(5));
+        }
+        @Test
+        public void testNonDividedByThreeOrFivePrintsTheNumber() {
+            assertEquals("fizzbuzz", JUnitTest.FizzBuzz(15));
+        }
+
+        @Test
+        public void testBiggerThan100ThenFalse(){
+        //given
+        int num = 98;
+        //when
+        boolean res = JUnitTest.smallerThanZero(num);
+        //then
+        assertFalse(res);
+
+        }
 }
